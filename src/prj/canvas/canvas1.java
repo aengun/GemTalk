4ad7ea87c7b92f77d006852d65c09d036fbc89a7
KIM SEUNGHYUN <sevencuter@gmail.com>
2020-10-16 22:51:42 +0900

    프로젝트1일(Card 구조작성)

---
+++ src/prj/canvas/canvas1.java
@@ -0,0 +1,5 @@
+package prj.canvas;
+
+public class canvas1 {
+
+}
---
+++ src/prj/entity/ChanceCard.java
@@ -0,0 +1,9 @@
+package prj.entity;
+
+public class ChanceCard extends Card {
+
+	public ChanceCard() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
